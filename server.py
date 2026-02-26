7d8692d754f5690d82e65b0618fc5e1cb7f0500c
eriomo <trigxyfn@gmail.com>
2026-02-26 15:26:37 +0100

    Update server.py

--- server.py
+++ server.py
@@ -41,9 +41,10 @@ def generate():
 
 
 # IMPORTANT: Do NOT use fixed port
+from flask import Flask, request, jsonify, render_template
+
+app = Flask(__name__)
+
 @app.route("/")
 def home():
-    return "LectureAI Backend is Running 🚀"
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
+    return render_template("index.html")
